38e9134ef7b8e8f4c1df98c05b4f1bb487c5148a
Daniel Foo <danielvundy@gmail.com>
2020-02-13 14:06:08 +0800

    Initial commit. Authenticate POST endpoint skeleton

---
+++ YkUser/YkUser/Controllers/AuthenticateController.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using YkUser.Model;
+
+namespace YkUser.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthenticateController : ControllerBase
+    {
+        // GET: api/Authenticate
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2", "value3" };
+        }
+
+        // GET: api/Authenticate/5
+        [HttpGet("{id}", Name = "Get")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST: api/Authenticate
+        [HttpPost]
+        public User Post([FromBody] User user)
+        {
+            return new User
+            {
+                Id = user.Id,
+                Admin = true,
+                Name = "Daniel - Authenticated"
+            };
+        }
+
+        // PUT: api/Authenticate/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE: api/ApiWithActions/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
